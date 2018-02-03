7b9bcd635c646c9624bb02085fc519d5649f4fdb
Yassin Hassan <yh@yassinhassan.com>
2018-02-03 19:35:19 +0100

    First delivrable

--- index.js
+++ index.js
@@ -110,146 +110,229 @@ request(url, function (error, response, html) {
 })
 */
 
-restaurants = getAllRestaurants();
-console.log(restaurants);
-data = JSON.stringify(restaurants);
-fs.writeFileSync('restaurants.json', data);
+
+//console.log("GET : " + get());
+
+async function get() {
+    await getAllRestaurants()
+        .then(function(result) { // returns a promise
+            //console.log(result);
+            data = JSON.stringify(result);
+            //fs.writeFileSync('restaurants.json', data);
+            resolve(result);
+        })
+        .catch(function(result) {
+            // Or do something else if it is rejected
+            // (it would not happen in this example, since `reject` is not called).
+        });
+}
+
+
+function delay(time) {
+    // `delay` returns a promise
+    return new Promise(function(resolve, reject) {
+        // Only `delay` is able to resolve or reject the promise
+        setTimeout(function() {
+            resolve(true); // After 60 seconds, resolve the promise with value true
+        }, 10000);
+    });
+}
 
 function getAllRestaurants(){
-    var all_restaurants = [];
-    var numberOfPages = getNumberOfPages();
-    console.log(numberOfPages);
-    var restaurants_url = getAllRestaurantsURL(numberOfPages);
-    console.log(restaurants_url);
-    var i;
-    for (i = 0; i < restaurants_url.length; i++) {
-        console.log('Number of restaurants : ' + all_restaurants.length);
-        all_restaurants = all_restaurants.concat( getRestaurant(restaurants_url[i]) );
-    }
+    return new Promise(function(resolve, reject) {
+        var all_restaurants = [];
+        var numberOfPages;
+        getNumberOfPages()
+            .then(function(result) { // returns a promise
+                //console.log("RES : " + result); // Log the value once it is resolved
+                numberOfPages = 1;//result;
+                getAllRestaurantsURL(numberOfPages)
+                    .then(function(result) { // returns a promise
+                        var restaurants_url = result;
+                        //console.log("RETOUR : " + restaurants_url);
 
-    return all_restaurants;
+                        var i;
+                        for (i = 0; i < restaurants_url.length; i++) {
+                            getRestaurant(restaurants_url[i])
+                                .then(function(result) { // returns a promise
+                                    all_restaurants = all_restaurants.concat( result );
+                                    console.log('Number of restaurants : ' + all_restaurants.length);
+                                })
+                                .catch(function(result) {
+                                    // Or do something else if it is rejected
+                                    // (it would not happen in this example, since `reject` is not called).
+                                });
+                        }
+
+                        delay(10*60*1000)
+                            .then(function(v) { // `delay` returns a promise
+                                console.log("DONE : GETTING ALL RESTAURANTS.");
+                                resolve(all_restaurants);
+                            })
+                            .catch(function(v) {
+                                // Or do something else if it is rejected
+                                // (it would not happen in this example, since `reject` is not called).
+                            });
+
+                    })
+                    .catch(function(result) {
+                        // Or do something else if it is rejected
+                        // (it would not happen in this example, since `reject` is not called).
+                    });
+            })
+            .catch(function(result) {
+                // Or do something else if it is rejected
+                // (it would not happen in this example, since `reject` is not called).
+            });
+    });
 }
 
 function getRestaurant(url) {
-    var restaurant = null;
-    request(url, function (error, response, html) {
-        if(error){
-            console.log(error);
-        } else if ( response.statusCode != 200) {
-            console.log(response.statusCode);
-        } else {
-            var $ = cheerio.load(html);
+    return new Promise(function(resolve, reject) {
+        var restaurant = null;
+        request(url, function (error, response, html) {
+            if(error){
+                console.log(error);
+            } else if ( response.statusCode != 200) {
+                console.log(response.statusCode);
+            } else {
+                var $ = cheerio.load(html);
 
-            var restaurantName = $('h1');
-            restaurantName = restaurantName.first();
-            //console.log(restaurantName.text());
+                var restaurantName = $('h1');
+                restaurantName = restaurantName.first();
+                //console.log(restaurantName.text());
 
-            var street = $('.thoroughfare');
-            console.log(street.length);
-            street = street.first();
-            //console.log(street.text());
+                var street = $('.thoroughfare');
+                console.log(street.length);
+                street = street.first();
+                //console.log(street.text());
 
-            var postalCode = $('.postal-code');
-            postalCode = postalCode.first();
-            //console.log(postalCode.text());
+                var postalCode = $('.postal-code');
+                postalCode = postalCode.first();
+                //console.log(postalCode.text());
 
-            var locality = $('.locality');
-            locality = locality.first();
-            //console.log(locality.text());
+                var locality = $('.locality');
+                locality = locality.first();
+                //console.log(locality.text());
 
-            var stars = 0;
-            var i = 1;
-            var numberOfStars = 0;
-            while( (stars.length == null || stars.length == 0) && i < 4){
-                if(i == 1){
-                    stars = $('.guide .icon-cotation1etoile ');
-                    if(stars.length > 0){
-                        numberOfStars = 1;
+                var stars = 0;
+                var i = 1;
+                var numberOfStars = 0;
+                while( (stars.length == null || stars.length == 0) && i < 4){
+                    if(i == 1){
+                        stars = $('.guide .icon-cotation1etoile ');
+                        if(stars.length > 0){
+                            numberOfStars = 1;
+                        }
                     }
-                }
-                if(i == 2){
-                    stars = $('.guide .icon-cotation2etoiles ');
-                    if(stars.length > 0){
-                        numberOfStars = 2;
+                    if(i == 2){
+                        stars = $('.guide .icon-cotation2etoiles ');
+                        if(stars.length > 0){
+                            numberOfStars = 2;
+                        }
                     }
-                }
-                if(i == 3){
-                    stars = $('.guide .icon-cotation3etoiles ');
-                    if(stars.length > 0){
-                        numberOfStars = 3;
+                    if(i == 3){
+                        stars = $('.guide .icon-cotation3etoiles ');
+                        if(stars.length > 0){
+                            numberOfStars = 3;
+                        }
                     }
+                    i++;
                 }
-                i++;
+
+                restaurant = {
+                    name: restaurantName.text(),
+                    stars: numberOfStars,
+                    street: street.text(),
+                    postalCode : postalCode.text(),
+                    locality: locality.text()
+                }
+
+                resolve(restaurant);
             }
-
-            restaurant = {
-                name: restaurantName.text(),
-                stars: numberOfStars,
-                street: street.text(),
-                postalCode : postalCode.text(),
-                locality: locality.text()
-            }
-
-            //console.log(restaurant);
-        }
-    })
-
-    return restaurant;
+        })
+    });
 }
 
 function getAllRestaurantsURL(numberOfPages) {
-    var restaurants_url = [];
-    var i;
-    for (i = 0; i < numberOfPages; i++) {
-        restaurants_url = restaurants_url.concat( getRestaurantsURL(i) );
-    }
-    return restaurants_url;
+    return new Promise(function(resolve, reject) {
+        var restaurants_url = [];
+        var i;
+        for (i = 0; i < numberOfPages; i++) {
+            getRestaurantsURL(i+1)
+                .then(function(result) { // returns a promise
+                    //console.log("RES : " + result); // Log the value once it is resolved
+                    restaurants_url = restaurants_url.concat( result );
+                })
+                .catch(function(result) {
+                    // Or do something else if it is rejected
+                    // (it would not happen in this example, since `reject` is not called).
+                });
+        }
+        console.log("ALL REQUEST LAUNCHED.");
+
+        delay(60*1000)
+            .then(function(v) { // `delay` returns a promise
+                console.log("DONE : GETTING ALL URL.");
+                //console.log(restaurants_url);
+                resolve(restaurants_url);
+            })
+            .catch(function(v) {
+                // Or do something else if it is rejected
+                // (it would not happen in this example, since `reject` is not called).
+            });
+    });
 }
 
 function getRestaurantsURL(pageNumber) {
-    var url = 'https://restaurant.michelin.fr/search-restaurants?stars=1||2||3&page_number=' + pageNumber;
-    var list_of_url = [];
-    request(url, function (error, response, html) {
-        if(error){
-            console.log(error);
-        } else if ( response.statusCode != 200) {
-            console.log(response.statusCode);
-        } else {
-            json_object = JSON.parse(html);
-            var $ = cheerio.load(json_object[3].data.search_result);
-            var links = $('.poi-card-link');
-            links.each(function( index ) {
-                //console.log( index + ": " + $( this ).attr( 'href' ) );
-                list_of_url.push('https://restaurant.michelin.fr' + $( this ).attr( 'href' ));
-            });
-        }
-    })
-    return list_of_url;
+    return new Promise(function(resolve, reject) {
+        console.log("Getting restaurants URL at page #" + pageNumber + " .");
+        var url = 'https://restaurant.michelin.fr/search-restaurants?stars=1||2||3&page_number=' + pageNumber;
+        var list_of_url = [];
+        request(url, function (error, response, html) {
+            if(error){
+                console.log(error);
+            } else if ( response.statusCode != 200) {
+                console.log(response.statusCode);
+            } else {
+                json_object = JSON.parse(html);
+                var $ = cheerio.load(json_object[3].data.search_result);
+                var links = $('.poi-card-link');
+                links.each(function( index ) {
+                    //console.log( index + ": " + $( this ).attr( 'href' ) );
+                    list_of_url.push('https://restaurant.michelin.fr' + $( this ).attr( 'href' ));
+                });
+                console.log("DONE : getting restaurants URL at page #" + pageNumber + " .");
+                resolve(list_of_url);
+            }
+        })
+    });
 }
 
 function getNumberOfPages() {
-    var url = 'https://restaurant.michelin.fr/search-restaurants?stars=1||2||3&page_number=1';
-    var numberOfPages = 0;
-    request(url, function (error, response, html) {
-        if(error){
-            console.log(error);
-        } else if ( response.statusCode != 200) {
-            console.log(response.statusCode);
-        } else {
-            json_object = JSON.parse(html);
-            var $ = cheerio.load(json_object[3].data.search_result);
-            var numbers = $('.mr-pager-link');
-            numbers.each(function( index ) {
-                if(parseInt( $( this ).attr( 'attr-page-number' ) ) > numberOfPages){
-                    numberOfPages = parseInt( $( this ).attr( 'attr-page-number' ) );
-                }
-                //console.log( index + ": " + $( this ).attr( 'attr-page-number' ) );
-            });
-            //console.log("Number of pages : " + numberOfPages);
-        }
-    })
-    console.log(' ::: ' +numberOfPages);
-    return numberOfPages;
+    return new Promise(function(resolve, reject) {
+        var url = 'https://restaurant.michelin.fr/search-restaurants?stars=1||2||3&page_number=1';
+        var numberOfPages = 0;
+        request(url, function (error, response, html) {
+            if(error){
+                console.log(error);
+            } else if ( response.statusCode != 200) {
+                console.log(response.statusCode);
+            } else {
+                json_object = JSON.parse(html);
+                var $ = cheerio.load(json_object[3].data.search_result);
+                var numbers = $('.mr-pager-link');
+                numbers.each(function( index ) {
+                    if(parseInt( $( this ).attr( 'attr-page-number' ) ) > numberOfPages){
+                        numberOfPages = parseInt( $( this ).attr( 'attr-page-number' ) );
+                    }
+                    //console.log( index + ": " + $( this ).attr( 'attr-page-number' ) );
+                });
+                //console.log("Number of pages : " + numberOfPages);
+                resolve(numberOfPages);
+            }
+        })
+    });
 }
 
 app.listen(port);
